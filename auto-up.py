6666f9e9162a286b551f533d437135a1c72e99a3
DaiPei <daipei@hust.edu.cn>
2018-05-07 10:17:39 +0800

    update notes

--- auto-up.py
+++ auto-up.py
@@ -5,10 +5,11 @@ from threading import Timer
 minutes = 15
 
 def upTask():
-    os.system('git pull')
-    os.system('git add --all')
-    os.system('git commit -m \'update notes\'')
-    os.system('git push')
+    # os.system('git pull')
+    # os.system('git add --all')
+    # os.system('git commit -m \'update notes\'')
+    # os.system('git push')
+    os.system('./up.sh')
     Timer(60 * minutes, upTask).start()
 
 upTask()
