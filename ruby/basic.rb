3852cf5a1cf0b5fe67a724d04dde6de167520720
DaiPei <daipei@hust.edu.cn>
2018-08-18 22:25:08 +0800

    update notes

--- ruby/basic.rb
+++ ruby/basic.rb
@@ -1,4 +1,9 @@
 
+=begin
+this is comment
+this is comment
+=end
+
 # Everything, including a literal, is an object, so this works:
 -199.abs                                                # 199
 "ruby is cool".length                                   # 12
